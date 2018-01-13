de44aad51e16550a7d74c3437fb20b55fabaa7a8
Seho Young <gnuoyohes@gmail.com>
2018-01-12 23:38:07 -0500

    added algorithm2.py

---
+++ algorithm2.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+#
+# Implements the Deep Q-Learning Algorithm as shown in the DeepMind paper
+
+import sys
+import atari_py
+import numpy as np
+from action_value_function import ActionValueFunction
+from network import network
+import random
+import matplotlib.pyplot as plt
+import pygame
+
+# set parameters, these are in the paper
+REPLAY_MEMORY_SIZE = 10000
+REPLAY_MINIBATCH_SIZE = 32
+AGENT_HISTORY_LENGTH = 4
+TARGET_NETWORK_UPDATE_FREQUENCY = 10000
+DISCOUNT_FACTOR = 0.99
+INITIAL_EXPLORATION = 1.0
+FINAL_EXPLORATION = 0.1
+FINAL_EXPLORATION_FRAME = 1000000
+REPLAY_START_SIZE = 100  # int(REPLAY_MEMORY_SIZE / 50)
+NUM_EPISODES = 10  # 100
+
+def show_screen(screen_data, screen):
+    screen_data_rot = np.flipud(np.rot90(screen_data))
+    screen.blit(pygame.pixelcopy.make_surface(screen_data_rot), (0, 0))
+    pygame.display.flip()
+
+# initialize ALE interface
+ale = atari_py.ALEInterface()
+pong_path = atari_py.get_game_path('breakout')
+ale.loadROM(pong_path)
+legal_actions = ale.getMinimalActionSet()
+num_of_actions = len(legal_actions)
+(screen_width, screen_height) = ale.getScreenDims()
+screen_data = np.zeros((screen_height, screen_width, 3),
+    dtype=np.uint8)  # Using RGB
+
+# initialize pygame
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Arcade Learning Environment Random Agent Display")
+pygame.display.flip()
+
+state1 = np.zeros((AGENT_HISTORY_LENGTH, screen_height,
+                   screen_width, 3), dtype=np.uint8)
+state2 = np.zeros((AGENT_HISTORY_LENGTH, screen_height,
+                   screen_width, 3), dtype=np.uint8)
+
+# observe initial state
+a = np.random.choice(legal_actions)
+print(a)
+for i in range(AGENT_HISTORY_LENGTH):
+    ale.act(a)
+    ale.getScreenRGB(screen_data)
+    show_screen(screen_data, screen)
+    state1[i] = np.copy(screen_data)
+
+# initialize replay memory D
+D = []
+for i in range(REPLAY_START_SIZE):
+    is_game_over = 0
+    a = np.random.choice(legal_actions)
+    print(a)
+    for j in range(AGENT_HISTORY_LENGTH):
+        r = ale.act(a)
+        if (ale.game_over()):
+            is_game_over = 1
+        ale.getScreenRGB(screen_data)
+        show_screen(screen_data, screen)
+        state2[j] = np.copy(screen_data)
+    D.append((np.copy(state1), a, r, np.copy(state2), is_game_over))
+    state1 = np.copy(state2)
+
+# initialize action-value function Q with random weights and its target clone
+net = network(screen_height, screen_width, num_of_actions)
+
+# main loop
+episode = 0
+step = 0
+e = INITIAL_EXPLORATION
+e_decrease = (INITIAL_EXPLORATION - FINAL_EXPLORATION) / \
+    FINAL_EXPLORATION_FRAME
+while (episode < NUM_EPISODES):
+    exit = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+            break
+    if(exit):
+        break
+
+    is_game_over = 0
+
+    # select an action a
+    if (np.random.sample() < e):
+        action = np.random.choice(legal_actions)
+    else:
+        action = np.argmax(net.evaluate(state1))
+
+    print(action)
+
+    # carry out action and observe reward
+    for i in range(AGENT_HISTORY_LENGTH):
+        reward = ale.act(action)
+        if (ale.game_over()):
+            is_game_over = 1
+        ale.getScreenRGB(screen_data)
+        show_screen(screen_data, screen)
+        state2[i] = np.copy(screen_data)
+
+    # store transition <s, a, r, s'> in replay memory D
+    if (len(D) == REPLAY_MEMORY_SIZE):
+        D.pop(0)
+    D.append((np.copy(state1), action, reward, np.copy(state2), is_game_over))
+
+    # sample random transitions <ss, aa, rr, ss'> from replay memory D
+    D_sample = random.sample(D, REPLAY_MINIBATCH_SIZE)
+
+    # calculate target for each minibatch transition
+    loss = []
+    for sample in D_sample:
+        q_target = net.evaluate(net.preprocess(sample[3]))
+        r = sample[2]
+        if (sample[4]):
+            t = r
+        else:
+            t = r + DISCOUNT_FACTOR * max(q_target)
+        # network.backpropagate((t - values[sample[1]]) ** 2)
+        loss.append(net.backpropagate(net.preprocess(sample[0]), q_target))
+        print(loss[-1])
+
+    # plt.plot(loss)
+
+    state1 = np.copy(state2)
+
+    if e > FINAL_EXPLORATION:
+        e = e - e_decrease
+    step = step + 1
+
+    # if (step == TARGET_NETWORK_UPDATE_FREQUENCY):
+    #     Q_target = Q.copy()
+    #     step = 0
+
+    if (is_game_over):
+        ale.reset_game()
+        episode = episode + 1
